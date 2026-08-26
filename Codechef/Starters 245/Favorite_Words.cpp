ab1c06135d5351383ad3fde41fb4067d95dae46f
A8dullah-Al-Mamun <a8dullah.al.mamun@gmail.com>
2026-08-26 15:35:06 +0600

    third problem

---
+++ Codechef/Starters 245/Favorite_Words.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string S;
+    cin >> S;
+
+    if (S[0] == 'c' || S[3] == 'f')
+    {
+        cout << "Yes\n";
+    }
+    else
+    {
+        cout << "No\n";
+    }
+
+    return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    if (s[0] == 'c' || s[3] == 'f')
+    {
+        cout << "yes \n";
+    }
+    else
+    {
+        cout << "NO \n";
+    }
+
+    return 0;
+}
