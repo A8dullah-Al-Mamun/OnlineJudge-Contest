48f539910decbd006aec781ca06a94c56feebf0d
A8dullah-Al-Mamun <a8dullah.al.mamun@gmail.com>
2026-08-26 15:37:01 +0600

    First problem of startes 246

---
+++ Codechef/Starters 246/Point_Calculation.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A, B, C;
+
+    cin >> A >> B >> C;
+
+    int total_points = (A * 3) + (B * 1);
+
+    cout << total_points << endl;
+
+    return 0;
+}
