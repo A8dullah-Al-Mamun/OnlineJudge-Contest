ee6b04df618ca5e01c59e563412a0906649d115e
A8dullah-Al-Mamun <a8dullah.al.mamun@gmail.com>
2026-08-27 21:11:30 +0600

    Fourth problem

---
+++ Codechef/Starters 252/Mirror_Swap.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> a(2 * n);
+        for (int i = 0; i < 2 * n; i++) cin >> a[i];
+
+        long long ans = 0;
+
+        for (int i = 0; i < n; i++)
+            ans += max(a[i], a[2 * n - 1 - i]);
+
+        cout << ans << '\n';
+    }
+
+    return 0;
+}
