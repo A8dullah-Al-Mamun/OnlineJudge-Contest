4554feddeb828f575c68f282dff737df6ebb7ec2
A8dullah-Al-Mamun <a8dullah.al.mamun@gmail.com>
2026-08-27 15:14:26 +0600

    First problem

---
+++ Codechef/Starters 253/Posh_Shopping.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<int> c(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> c[i];
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            ans = max(ans, c[i]);
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (c[i] <= c[j])
+                {
+                    ans = max(ans, c[i] + c[j]);
+                }
+            }
+        }
+
+        cout << ans << '\n';
+    }
+
+    return 0;
+}
